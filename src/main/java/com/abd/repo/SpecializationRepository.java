edb3f4aaddeac82831c82c54c96e0db49ed606bf
theabdul97@gmail.com <theabdul97@gmail.com>
2023-10-27 15:43:45 +0530

    repo

---
+++ src/main/java/com/abd/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package com.abd.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abd.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization,Long> {
+
+}
