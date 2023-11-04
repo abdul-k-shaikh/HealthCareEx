168066adfcc544e40177e088753e226b242f943e
theabdul97@gmail.com <theabdul97@gmail.com>
2023-11-04 16:15:13 +0530

    Register Form, validation at entity

--- src/main/java/com/abd/controller/SpecializationController.java
+++ src/main/java/com/abd/controller/SpecializationController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.abd.entity.Specialization;
@@ -17,9 +19,34 @@ public class SpecializationController {
 	
 	@Autowired
 	private ISpecializationService service;
+	/**
+	 * 1. Show register page
+	 * 
+	 * */
+	@GetMapping("/register")
+	public String displayRegister() {
+		return "SpecializationRegister";
+	}
 	
 	/**
-	 * 1. Display all specialization
+	 * 2. On submit form save data
+	 * 
+	 * */
+	@PostMapping("/save")
+	public String saveForm(
+			@ModelAttribute Specialization specialization, Model model
+			)
+	{    
+		Long saveSpecializationId = service.saveSpecialization(specialization);
+		String message = "Record ("+saveSpecializationId+") is created";
+		model.addAttribute("message", message);
+		return "SpecializationRegister";
+		
+	}
+	
+	
+	/**
+	 * 3. Display all specialization
 	 * 
 	 * */
 	@GetMapping("/all")
